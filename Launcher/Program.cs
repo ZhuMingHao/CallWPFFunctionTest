9e76be0964c357811c47b316894660813c814071
Nico Zhu <v-zhumin@microsoft.com>
2021-06-21 14:05:08 +0800

    Add project files.

---
+++ Launcher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Launcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            if (args.Length > 2)
+            {
+                if (args[2].ToString() == "/extension")
+                {
+                    TestMethod();
+                }
+            }
+        }
+        private static void TestMethod()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
